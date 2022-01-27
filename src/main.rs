a42f11a9abc51bebd2a2e8f1373c6971651a4911
pigeon-king-17 <luke.mill2025@student.rugbyschool.ac.th>
2022-01-27 21:09:29 +0700

    added render and random_state functions

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,61 @@
 struct Board {
-    board: Vec<Vec<bool>>
+    board: Vec<Vec<bool>>,
+    width: usize,
+    height: usize,
 }
 
 impl Board {
-    fn dead_state(width: usize, height: usize) -> Vec<Vec<bool>> {
+    fn dead_state(width: usize, height: usize) -> Board {
         let mut board = Vec::new();
 
         for _row in 0..height {
             board.push(vec![false; width]);
         }
 
-        board
+        Board {
+            board,
+            width,
+            height,
+        }
+    }
+
+    fn random_state(width: usize, height: usize) -> Board {
+        let mut board = Vec::new();
+
+        for _row in 0..height {
+            let mut board_row: Vec<bool> = Vec::new();
+            for _cell in 0..width {
+                board_row.push(rand::random());
+            }
+            board.push(board_row);
+        }
+
+        Board {
+            board,
+            width,
+            height,
+        }
+    }
+
+    fn render(&self) {
+        println!("+{:-<1$}+", "", self.width);
+        for row in 0..self.height {
+            print!("|");
+            for cell in &self.board[row] {
+                match cell {
+                    true => print!("#"),
+                    false => print!(" "),
+                    _ => (),
+                };
+            }
+            print!("|\n");
+        }
+        println!("+{:-<1$}+", "", self.width);
     }
 }
 
 fn main() {
-    let board = Board::dead_state(10, 10);
+    let board = Board::random_state(80, 30);
 
-    println!("{:#?}", board)
+    board.render();
 }
