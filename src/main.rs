70af83eaa68464dbb16936e77d39ba44fd8fa4a2
pigeon-king-17 <luke.mill2025@student.rugbyschool.ac.th>
2022-01-30 09:33:38 +0700

    completed basic implementation

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-struct Board {
+use std::{thread, time};
+
+pub struct Board {
     board: Vec<Vec<bool>>,
     width: usize,
     height: usize,
 }
 
 impl Board {
-    fn dead_state(width: usize, height: usize) -> Board {
+    pub fn dead_state(&self, width: usize, height: usize) -> Board {
         let mut board = Vec::new();
 
         for _row in 0..height {
@@ -19,7 +21,7 @@ impl Board {
         }
     }
 
-    fn random_state(width: usize, height: usize) -> Board {
+    pub fn random_state(width: usize, height: usize) -> Board {
         let mut board = Vec::new();
 
         for _row in 0..height {
@@ -37,7 +39,7 @@ impl Board {
         }
     }
 
-    fn render(&self) {
+    pub fn render(&self) {
         println!("+{:-<1$}+", "", self.width);
         for row in 0..self.height {
             print!("|");
@@ -45,17 +47,71 @@ impl Board {
                 match cell {
                     true => print!("#"),
                     false => print!(" "),
-                    _ => (),
                 };
             }
             print!("|\n");
         }
         println!("+{:-<1$}+", "", self.width);
     }
+
+    pub fn next_board_state(&self) -> Board {
+        let mut new_board = self.dead_state(self.width, self.height);
+
+        for x_pos in 0..self.width {
+            for y_pos in 0..self.height {
+                let neighbours = get_neighbours(x_pos, y_pos, &self);
+                let num_trues = neighbours.iter().filter(|x| x == &&true).count();
+
+                if num_trues < 2 {
+                    new_board.board[y_pos][x_pos] = false;
+                } else if (num_trues == 2 || num_trues == 3) && self.board[y_pos][x_pos] == true {
+                    new_board.board[y_pos][x_pos] = true;
+                } else if num_trues == 3 && self.board[y_pos][x_pos] == false {
+                    new_board.board[y_pos][x_pos] = true;
+                } else if num_trues > 3 {
+                    new_board.board[y_pos][x_pos] = false;
+                }
+            }
+        }
+        
+        new_board
+    }
 }
 
 fn main() {
-    let board = Board::random_state(80, 30);
+    let mut board = Board::random_state(80, 30);
 
-    board.render();
+    for _i in 0..100 {
+        board.render();
+        board = board.next_board_state();
+        thread::sleep(time::Duration::from_millis(250));
+    }
+}
+
+fn get_neighbours(x_pos: usize, y_pos: usize, board: &Board) -> Vec<bool> {
+    let neighbours: Vec<bool>;
+    let width = &board.width;
+    let height = &board.height;
+
+    if x_pos == 0 && y_pos == 0 {
+        neighbours = vec!(board.board[0][1], board.board[1][1], board.board[1][0])
+    } else if x_pos == 0 && y_pos == height-1 {
+        neighbours = vec!(board.board[y_pos-1][0], board.board[y_pos][1], board.board[y_pos-1][1])
+    } else if x_pos == width-1 && y_pos == 0 {
+        neighbours = vec!(board.board[1][x_pos], board.board[0][x_pos-1], board.board[1][x_pos-1])
+    } else if x_pos == width-1 && y_pos == height-1 {
+        neighbours = vec!(board.board[y_pos][x_pos-1], board.board[y_pos-1][x_pos], board.board[y_pos-1][x_pos-1])
+    } else if x_pos == 0 {
+        neighbours = vec!(board.board[y_pos-1][0], board.board[y_pos+1][0], board.board[y_pos][1], board.board[y_pos+1][1], board.board[y_pos-1][1])
+    } else if x_pos == width-1 {
+        neighbours = vec!(board.board[y_pos-1][x_pos], board.board[y_pos+1][x_pos], board.board[y_pos][x_pos-1], board.board[y_pos-1][x_pos-1], board.board[y_pos+1][x_pos-1])
+    } else if y_pos == 0 {
+        neighbours = vec!(board.board[y_pos][x_pos-1], board.board[y_pos][x_pos+1], board.board[y_pos+1][x_pos], board.board[y_pos+1][x_pos+1], board.board[y_pos+1][x_pos-1])
+    } else if y_pos == height-1 {
+        neighbours = vec!(board.board[y_pos][x_pos-1], board.board[y_pos][x_pos+1], board.board[y_pos-1][x_pos], board.board[y_pos-1][x_pos+1], board.board[y_pos-1][x_pos-1])
+    } else {
+        neighbours = vec!(board.board[y_pos-1][x_pos-1], board.board[y_pos][x_pos-1], board.board[y_pos+1][x_pos-1], board.board[y_pos-1][x_pos], board.board[y_pos+1][x_pos], board.board[y_pos-1][x_pos+1], board.board[y_pos][x_pos+1], board.board[y_pos+1][x_pos+1])
+    }
+
+    neighbours
 }
